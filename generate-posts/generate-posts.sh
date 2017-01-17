ad22bf1f943ad71df378df712208109a75c776e3
John D. Martin III <john.d.martin.iii@gmail.com>
2017-01-17 16:05:52 -0500

    Update live site Tue Jan 17 16:05:51 EST 2017

--- generate-posts/generate-posts.sh
+++ generate-posts/generate-posts.sh
@@ -12,6 +12,7 @@ date: '$DATE'
 category: '$CATEGORY' 
 title: '$TITLE' 
 tags: [$TAGS] 
+noreading: true
 --- 
 
 <excerpt/>" > $DATE-`echo "$TITLE" | iconv -t ascii//TRANSLIT | sed -r s/[^a-zA-Z0-9]+/-/g | sed -r s/^-+\|-+$//g | tr A-Z a-z`.md
